ff625eadb14a548b476fdd04101c7dcf9560cfb8
abdullah-mamun <aamun22@gmail.com>
2022-07-10 14:39:12 +0300

    class 5 added

---
+++ 5th/main.js
@@ -0,0 +1,24 @@
+// var num = 20;
+// toString(num);
+// document.write(num);
+// var string_ = toString(num);
+// document.write(typeof(string_));
+
+// var num = "20";
+// num = parseInt(num);
+
+// document.write(typeof(num));
+
+// var num = "20.5";
+// num = parseFloat(num);
+
+// document.write(typeof(num));
+// var number = 2.57893;
+// document.write(number.toFixed(2));
+
+// document.write(number.toPrecision(1));
+
+// document.write(Number("12"));
+// document.write(typeof(Number("12")));
+document.write(Number(true));
+document.write(Number(false));
