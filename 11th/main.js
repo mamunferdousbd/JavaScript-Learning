be8aea598f216c20e48f70fa196aeaa34ca2d0e3
abdullah-mamun <aamun22@gmail.com>
2022-07-12 09:36:51 +0300

    class 11 Added

---
+++ 11th/main.js
@@ -0,0 +1,10 @@
+// Farenheit
+
+//  var cel = parseFloat(prompt("Enter The Celcius Value:"));
+//  var far = 9 / 5 * (cel) + 32;
+//  document.write("Farenhite: " + far + "<br>");
+
+// Celcius
+var farnen = parseFloat(prompt("Enter The farenheit Value:"));
+var celcius = (5 / 9) * (farnen - 32);
+document.write("Celcius: " + celcius + "<br>");
