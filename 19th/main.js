9562173a00803bf4a5058d49503d2fa1f1f9198e
abdullah_mamun <aamun22@gmail.com>
2022-07-12 23:42:38 -0700

    Class 17 transfer to class 19

---
+++ 19th/main.js
@@ -0,0 +1,39 @@
+
+
+for( var x = 1; x <=10; x++){
+    document.write("Bangladesh " + x +"<br>");
+   
+}
+document.write("<h2>End</h2>");
+
+
+// 1 3 5 7 9....99(Increment)
+
+for (var n =1 ; n <=99 ; n+=2){
+document.write(" " + n);
+}
+document.write("<h2>End</h2>");
+
+// 0 2 4 6 8....100(Increment)
+for ( var i =0 ; i<=100 ;i+=2){
+document.write(" "+ i);
+}
+document.write("<h2>End</h2>");
+
+// 10 9 8 7 6 5 4 3 2 1
+
+for( var p = 10; p >=1; p = p-1 ){
+    document.write(" "+ p);
+}
+document.write("<h2>End</h2>");
+// 10  8  6  4  2 
+for( var p = 10; p >=1; p = p-2 ){
+    document.write(" "+ p);
+}
+document.write("<h2>End</h2>");
+// 19 17 15 13 11 9 7 5 3 1 
+for(var x= 19; x>=1; x=x-2){
+    document.write(" "+ x);
+}
+document.write("<h2>End</h2>");
+document.write("<h2>End</h2>");
