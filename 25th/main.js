8ed05b739f4b1e95caeb60efca67649b700381ad
abdullah_mamun <aamun22@gmail.com>
2022-07-14 07:31:18 -0700

    class 25th Added IIFEs

---
+++ 25th/main.js
@@ -0,0 +1,16 @@
+
+// IIFEs
+ (function addition(num1,num2){
+        var sum = num1 +num2;
+        document.write(sum+"<br>");
+ })(10000,20);
+
+//  Function Expression
+const multi = function multiplication(num1,num2){
+    var mul = num1 *num2;
+    document.write(" multiplication = "+mul+"<br>");
+}
+multi(10,20);
+
+
+// () => Round Brackets or Parentheses,{} => Curly brackets,[]=> Square Brackets or Brackets,<> =>Angle brackets
