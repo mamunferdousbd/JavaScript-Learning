899679bf226d5fd6f76c4ae6d037087bcbf12411
abdullah-mamun <aamun22@gmail.com>
2022-07-15 21:36:35 +0300

    class 32 For Math object

---
+++ 32th/main.js
@@ -0,0 +1,6 @@
+// Finding The Maximum Number with Math(Library) Function
+
+// var num1 = parseFloat(prompt("Enter the first Number :"));
+// var num2 = parseFloat(prompt("Enter the second Number :"));
+var maximum = Math.max(num1, num2);
+document.write(" Maximum Number :" + maximum);
