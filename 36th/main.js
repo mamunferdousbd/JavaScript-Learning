0212935658e98d0cbf110014dc8cb5d16eb42353
abdullah_mamun <aamun22@gmail.com>
2022-07-16 10:47:14 -0700

    class 36 added Event Handler to onclick event

---
+++ 36th/main.js
@@ -0,0 +1,13 @@
+function Mybutton1(){
+    document.querySelector("p").innerHTML="You have Clickec on Buttton 1";
+}
+function Mybutton2(){
+    document.querySelector("p").innerHTML="You have Clickec on Buttton 2";
+}
+
+function Myimage1(){
+    document.querySelector("#pic").src ="image.jpg";
+}
+function Myimage2(){
+    document.querySelector("#pic").src ="image1.jpg";
+}
