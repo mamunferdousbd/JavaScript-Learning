4ad84ae58fafbc7cbb5949ed40d23723e3667609
abdullah_mamun <aamun22@gmail.com>
2022-07-16 08:17:09 -0700

    class 35 added with querySelector and querySelectorAll

---
+++ 35th/main.js
@@ -0,0 +1,11 @@
+
+// query Selector
+document.querySelector(".mamun").innerHTML=" Class name is mamun selected by Query Selector ";
+document.querySelector("#kamal").innerHTML=" Id  name is kamal selected by Query Selector ";
+document.querySelector("h1").innerHTML=" Tag  name  selected by Query Selector ";
+
+
+// 
+
+document.querySelector("a").innerHTML="bari";
+document.querySelectorAll("li")[2].innerHTML="bari";
