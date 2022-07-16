a1619e6922a88c2422f3e18fda938b49e38ab30e
abdullah_mamun <aamun22@gmail.com>
2022-07-16 07:35:23 -0700

    class 34 with DoM Added

---
+++ 34th/main.js
@@ -0,0 +1,17 @@
+// .getElementById
+
+// document.getElementById("demo").innerHTML = "This heading is selected by using getElementById and changed by innerHTML ";
+// document.getElementById("demo");
+// var headig1 = document.getElementById("demo1");
+// headig1.innerHTML="Good By Good By";
+
+// .getElementsByTagName(Must be include [index number])
+// var headingFirst = document.getElementsByTagName("h1")[0]; 
+// headingFirst.innerHTML= "Selected By getElementsByTagName";
+
+// getElementsByClassName
+
+document.getElementsByClassName("headingSecond")[0].innerHTML= "selected by getElementsByClassName ";
+
+
+
