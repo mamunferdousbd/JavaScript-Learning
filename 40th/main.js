822e430842b0f5a615d940816563fe4bc4d3ed55
abdullah-mamun <aamun22@gmail.com>
2022-07-17 17:56:59 +0300

    class 40 added with classList.add and classList.remove function

---
+++ 40th/main.js
@@ -0,0 +1,16 @@
+function addStyle() {
+    var heading = document.querySelector("h2");
+    heading.classList.add("heading");
+    // heading.style.color = "red";
+    // heading.style.fontSize = "32px";
+    // heading.style.fontWeight = "900";
+    // heading.style.fontStyle = "italic";
+
+
+}
+
+function removeStyle() {
+    var heading = document.querySelector("h2");
+    heading.classList.remove("heading");
+
+}
