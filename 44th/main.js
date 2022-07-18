20347ea8a225b804eb07b95d03e9f73a921bdc8c
abdullah_mamun <aamun22@gmail.com>
2022-07-18 11:06:50 -0700

    audio and animation added

---
+++ 44th/main.js
@@ -0,0 +1,44 @@
+
+for(var i = 0; i <3; i++){
+    document.querySelectorAll(".mybtn")[i].addEventListener("click",function(){
+    var text = this.innerHTML;
+    var text= text.toLowerCase();
+    console.log(text);
+    audioPlay(text);
+    playAnimation(text);
+        
+        
+    });
+}
+// by calling a function
+
+function audioPlay(text){
+switch(text){
+            case "a" :
+                    var audio = new Audio('sounds/a.mp3');
+                        audio.play();
+                        break;
+            case "s" :
+                    var audio = new Audio('sounds/s.mp3');
+                        audio.play();
+                        break;
+            case "t" :
+                    var audio = new Audio('sounds/t.mp3');
+                        audio.play();   
+                        break;         
+        }
+}
+
+function playAnimation(text){
+    var btnSelect = document.querySelector("."+text);
+    btnSelect.classList.add("animationStyle");
+
+    setTimeout(function(){
+        btnSelect.classList.remove("animationStyle");
+    },1000);
+    // setTimeout(() => {
+    //     btnSelect.classList.remove("animationStyle");
+    // }, 1000);
+
+}
+
