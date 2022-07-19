3ad78c24fd8a7a35abb2028b7463158d5ac5b7f9
abdullah-mamun <aamun22@gmail.com>
2022-07-19 21:07:18 +0300

    mouse event added

---
+++ 50th/main.js
@@ -0,0 +1,38 @@
+// Click
+console.clear();
+// var div = document.querySelector("div");
+// div.addEventListener('click', function() {
+// console.log("Click is Occured.");
+// });
+
+// Double click
+/*var div = document.querySelector("div");
+div.addEventListener('dblclick', function() {
+    console.log("Doubleclick is Occured.");
+});*/
+/*
+var div = document.querySelector("div");
+div.addEventListener('mousedown', function() {
+    console.log("Mousedown is Occured.");
+});*/
+/*
+var div = document.querySelector("div");
+div.addEventListener('mouseup', function() {
+    console.log("Mouseup is Occured.");
+});*/
+var div = document.querySelector("div");
+div.addEventListener('mouseenter', function() {
+    console.log("MouseEnter is occured");
+});
+var div = document.querySelector("div");
+div.addEventListener('mouseleave', function() {
+    console.log("MouseLeave is occured");
+});
+var div = document.querySelector("div");
+div.addEventListener('mousemove', function() {
+    console.log("MouseMove is occured");
+});
+var div = document.querySelector("div");
+div.addEventListener('mouseover', function() {
+    console.log("MouseOver is occured");
+});
