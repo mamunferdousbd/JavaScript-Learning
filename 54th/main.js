a54197310c7c8ec688c449b4f30f21ac371acb28
abdullah_mamun <aamun22@gmail.com>
2022-07-20 11:17:07 -0700

    DragEvent

---
+++ 54th/main.js
@@ -0,0 +1,14 @@
+var div = document.querySelector(".myDiv");
+var para = document.querySelector("#drag1");
+
+para.addEventListener('dragstart',function(e){
+    e.dataTransfer.setData('text', e.target.id);
+});
+div.addEventListener("dragover",function(e){
+    e.preventDefault();
+})
+div.addEventListener("drop",function(e){
+    var id = e.dataTransfer.getData('text');
+    console.log(id);
+    div.appendChild(document.getElementById(id))
+});
