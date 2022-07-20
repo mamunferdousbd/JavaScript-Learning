2b283deae9de3150111c43728b1fe1e586f12f1b
abdullah-mamun <aamun22@gmail.com>
2022-07-20 15:24:08 +0300

    preventdafult added

--- 54th/main.js
+++ 54th/main.js
@@ -1,14 +1,15 @@
 var div = document.querySelector(".myDiv");
 var para = document.querySelector("#drag1");
 
-para.addEventListener('dragstart',function(e){
+para.addEventListener('dragstart', function(e) {
     e.dataTransfer.setData('text', e.target.id);
 });
-div.addEventListener("dragover",function(e){
+div.addEventListener("dragover", function(e) {
     e.preventDefault();
 })
-div.addEventListener("drop",function(e){
+div.addEventListener("drop", function(e) {
     var id = e.dataTransfer.getData('text');
     console.log(id);
     div.appendChild(document.getElementById(id))
+    e.preventDefault();
 });
