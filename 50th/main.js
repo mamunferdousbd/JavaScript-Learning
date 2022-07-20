356f635a81cecaf213799b4b1dbf1d76462c4612
abdullah_mamun <aamun22@gmail.com>
2022-07-19 19:57:39 -0700

    Mouse Event Completed

--- 50th/main.js
+++ 50th/main.js
@@ -1,38 +1,104 @@
-// Click
 console.clear();
+
+/*================== 
+Click
+====================*/
+
 // var div = document.querySelector("div");
 // div.addEventListener('click', function() {
 // console.log("Click is Occured.");
 // });
 
-// Double click
-/*var div = document.querySelector("div");
-div.addEventListener('dblclick', function() {
-    console.log("Doubleclick is Occured.");
-});*/
-/*
-var div = document.querySelector("div");
-div.addEventListener('mousedown', function() {
-    console.log("Mousedown is Occured.");
-});*/
-/*
-var div = document.querySelector("div");
-div.addEventListener('mouseup', function() {
-    console.log("Mouseup is Occured.");
-});*/
-var div = document.querySelector("div");
-div.addEventListener('mouseenter', function() {
-    console.log("MouseEnter is occured");
+/*=================================
+Double click
+=================================*/ 
+// var div = document.querySelector("div");
+// div.addEventListener('dblclick', function() {
+    // console.log("Doubleclick is Occured.");
+// });
+
+/*============================= 
+Mousedown
+=============================*/
+// var div = document.querySelector("div");
+// div.addEventListener('mousedown', function() {
+    // console.log("Mousedown is Occured.");
+// });
+
+
+/*=======================================
+Mouseup
+=======================================*/
+// var div = document.querySelector("div");
+// div.addEventListener('mouseup', function() {
+    // console.log("Mouseup is Occured.");
+// });
+/*===============================
+MouseEnter
+===============================*/
+
+// var div = document.querySelector("div");
+// div.addEventListener('mouseenter', function() {
+//     console.log("MouseEnter is occured");
+// });
+
+/*===============================
+MouseLeave
+===============================*/
+
+// var div = document.querySelector("div");
+// div.addEventListener('mouseleave', function() {
+//     console.log("MouseLeave is occured");
+// });
+
+/*===============================
+Mousemove
+===============================*/
+
+
+// var div = document.querySelector("div");
+// div.addEventListener('mousemove', function() {
+//     console.log("MouseMove is occured");
+// });
+
+/*===============================
+MouseOver
+===============================*/
+// var div = document.querySelector("div");
+// div.addEventListener('mouseover', function() {
+//     console.log("MouseOver is occured");
+// });
+
+/*===============================
+Mousemove Details
+===============================*/
+
+
+// var div = document.querySelector("div");
+// div.addEventListener('mousemove',function(e){
+//     console.log(" OffsetX : "+e.offsetX +" , OffsetY :"+ e.offsetY);
+//     // console.log(" ClientX : "+e.clientX +" , ClientY :"+ e.clientY);
+// });
+
+/*======================= 
+Click Details
+=======================*/ 
+// var div = document.querySelector("div");
+// div.addEventListener('click',function(e){
+    //  console.log(e);
+    // console.log(e.target);
+    // console.log(e.target.id);
+    // console.log(e.target.className);
+    //  console.log(e.target.innerHTML);
+    //  console.log(e.target.textContent);
+    //  console.log(e.target.innerText);
+// });
+
+var buttons = document.querySelectorAll(".btn");
+
+Array.from(buttons).map((button) => {
+button.addEventListener('click',function(e){
+    // console.log(e.target.textContent);
+    console.log(e.target.innerHTML);
 });
-var div = document.querySelector("div");
-div.addEventListener('mouseleave', function() {
-    console.log("MouseLeave is occured");
-});
-var div = document.querySelector("div");
-div.addEventListener('mousemove', function() {
-    console.log("MouseMove is occured");
-});
-var div = document.querySelector("div");
-div.addEventListener('mouseover', function() {
-    console.log("MouseOver is occured");
 });
