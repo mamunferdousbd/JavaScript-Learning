167e64cb029aef2b129d56496403d2a0dc9cdd52
abdullah_mamun <aamun22@gmail.com>
2022-07-21 09:26:20 -0700

    how to handle error in JavaScript (part-1)

---
+++ 60th/main.js
@@ -0,0 +1,33 @@
+//  How to handle error in JavaScript (part-1)
+
+// error handling ----- try, catch, finally, through.
+// 
+// 
+// ==============Only for run time handel ==========
+// ==============Will not handle sytext error ==========
+
+/*==================================
+generate Error 
+==================================*/
+// alert("Hello how are you?"); //code will run
+// alert(X); // will not run because of error and program will stop ,not proceed next
+// alert("Hello, Everybody!"); // will not run this because of error catching
+
+try{
+
+    // Main code will be hare (where error may come)
+    alert("hello, This alert will run");
+    alert(x);
+    
+} catch(error){
+    // Handle Error
+    console.log("Inside Catch Block");
+    console.log(error);
+    console.log(error.name);
+    console.log(error.message);
+    console.log(error.stack);
+    
+    
+}finally{
+    alert("hello, This alert will run always");
+}
