32a468acb4aecf0dc46ab9cb6c5c57bb51b1d0fe
abdullah_mamun <aamun22@gmail.com>
2022-07-21 08:41:34 -0700

    best practices for javascript coding

---
+++ 59th/main.js
@@ -0,0 +1,34 @@
+// Best practices for javascript coding
+
+// Use camelCase for naming
+
+var firstName;
+
+//  Variable declearation on top and initialize
+ var firstName, userName, userEmail;
+ firstName ="";
+
+// Avoid unnecessary variables
+
+// switch ,case,default
+
+
+// switch(key){
+//     case value:
+//         break;
+//     default:
+//         break;
+// }
+// object array with const
+ const name =["abdullah","mamun","hasan"];
+
+// == VS==
+ var nam;
+ var mun2
+ nam === mun2;
+
+
+// Access dom less 
+
+
+console.log(document.querySelector(".order_list").children[1]) ;
