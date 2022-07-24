ffc0c6bea637b118cfe22ae1de5c03ea03f28283
abdullah-mamun <aamun22@gmail.com>
2022-07-24 16:57:05 +0300

    Hoisting and strict

---
+++ 65th/main.js
@@ -0,0 +1,13 @@
+/*==================
+ Hoisting
+==================*/
+
+// x = 20;
+// console.log(x);
+// For let and const it will show error.Hoisting is not possible with let and const
+/*==================
+ Strict
+==================*/
+"use strict";
+x = 10;
+console.log(x);
