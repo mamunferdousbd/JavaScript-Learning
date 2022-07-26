6f61aa13f6564ca90ed1b8d600808324fc487381
abdullah_mamun <aamun22@gmail.com>
2022-07-26 09:56:20 -0700

    string Include EndsWith and StartsWith

---
+++ 76th/main.js
@@ -0,0 +1,19 @@
+
+// String.startsWith()
+// The startsWith() method returns true if a string begins with a specified value, otherwise false:
+const text = "Hello world, welcome to the universe.";
+console.log(text.startsWith("Hello"));
+
+// String.endsWith()
+// The endsWith() method returns true if a string ends with a specified value, otherwise false:
+const fullName = "Abdullah Al Mamun";
+console.log(fullName.endsWith("Mamun"));
+
+
+//String.includes()
+// The includes() method returns true if a string contains a specified value, otherwise false:
+
+const message = " This is a Message";
+console.log(message.includes("a"));
+
+// Case Sensetive
